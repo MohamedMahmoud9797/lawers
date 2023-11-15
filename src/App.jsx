0f42d7a3eeb7db8a0b2e3075b431c141f303830e
Your Name <you@example.com>
2023-11-15 02:02:55 +0200

    add

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AccordionCopmonent from "./Components/Accordion";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <h1>Lawers</h1>
+      <AccordionCopmonent />
+    </>
+  );
+}
+
+export default App;
